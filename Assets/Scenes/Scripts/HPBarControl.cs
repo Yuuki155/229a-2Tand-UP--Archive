05d365a5e73689acdd1298f2cca1c4c90f078e9d
Taipingan <105578757+Taipingan@users.noreply.github.com>
2026-03-25 13:53:25 +0700

    UiHPBar

---
+++ Assets/Scenes/Scripts/HPBarControl.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBarControl : MonoBehaviour
+{
+    public Slider hpBar;
+    public PlayerBasicMovement player;
+    public float maxHP;
+    public float minHP;
+    public float currentHP;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        maxHP = player.maxStamina;
+        currentHP = maxHP;
+        hpBar.value = currentHP / maxHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //hpBar.value = currentHP / maxHP;
+        //currentHP = player.currentStamina;
+    }
+}
